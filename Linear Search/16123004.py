73188a8fa0838a90411c2a9a8eed25aa71c98ac6
Psycho-Pirate <prakharsaxena.civ18@iitbhu.ac.in>
2020-10-17 22:20:36 +0530

    Linear search updated

--- Linear Search/16123004.py
+++ Linear Search/16123004.py
@@ -1,8 +1,16 @@
 k = 4
 l = [ 2,3,4,5,6,8]
 
+flag = 0
 n = len(l)
-for i in n:
-	# TODO Fill Code here
 
+for i in range(n):
+	if l[i] == k:
+		flag = 1
+		pos = i
+		break 
 
+if flag == 0:
+	print("Element not present")
+else:
+	print("Element is present at index", str(pos))
