d3c5e87e401db0a7a13ee77b337320382004df0a
Psycho-Pirate <prakharsaxena.civ18@iitbhu.ac.in>
2020-10-17 22:17:26 +0530

    Binary Search Updated

--- Binary Search/16123004.py
+++ Binary Search/16123004.py
@@ -1,7 +1,32 @@
 l = [ 2,3,4,5,6,8]
 
 n = len(l)
-for i in n:
-	# TODO Fill Code here
 
+k = 4
+
+left = 0
+right = n - 1
+mid = 0
+
+flag = 0
+
+while left <= right: 
+
+    mid = (right + left) // 2
+
+    if l[mid] < k: 
+        left = mid + 1
+
+    elif l[mid] > k: 
+        right = mid - 1
+
+    else: 
+        flag = 1
+        pos = mid
+        break 
+
+if flag == 0:
+	print("Element not present")
+else:
+	print("Element is present at index", str(pos))
 
