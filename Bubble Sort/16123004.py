214b22116a8c68553cbe3245540489f4d658c81e
Psycho-Pirate <prakharsaxena.civ18@iitbhu.ac.in>
2020-10-17 22:22:53 +0530

    Bubble Sort updated

--- Bubble Sort/16123004.py
+++ Bubble Sort/16123004.py
@@ -1,7 +1,11 @@
 l = [ 2,3,4,5,6,8]
 
 n = len(l)
-for i in n:
-	# TODO Fill Code here
 
+for i in range(n-1):  
+        for j in range(n-i-1): 
+            if l[j] > l[j+1] : 
+                l[j], l[j+1] = l[j+1], l[j] 
 
+print ("Sorted Array:") 
+print (l)
